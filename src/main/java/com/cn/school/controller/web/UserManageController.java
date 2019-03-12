d7119d67078366343c7ba9ef4829b428afcc22f2
humin <humin@yazuo.com>
2019-03-12 11:53:18 +0800

    update

--- src/main/java/com/cn/school/controller/web/UserManageController.java
+++ src/main/java/com/cn/school/controller/web/UserManageController.java
@@ -1,6 +1,12 @@
 package com.cn.school.controller.web;
 
+import com.cn.school.dto.forms.user.UserInsertViewForm;
+import com.cn.school.utils.request.RestRequest;
+import com.cn.school.utils.response.RestResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,8 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/web/user")
 public class UserManageController {
 
-    @RequestMapping("/login")
-    public String index() {
-        return "Hello World";
+    /**
+     * 发送短信验证码（所有手机号都可以发送）
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/getList", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public RestResponse sendCode(@RequestBody RestRequest<UserInsertViewForm> request) {
+        UserInsertViewForm viewForm = request.getBody();
+        return null;
     }
+
 }
