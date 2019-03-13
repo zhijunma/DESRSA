863c4ca08f3cf651d63022dc7be57a72871d7c9f
humin <humin@yazuo.com>
2019-03-13 17:21:58 +0800

    add

--- src/main/java/com/cn/school/SchoolApplication.java
+++ src/main/java/com/cn/school/SchoolApplication.java
@@ -2,9 +2,11 @@ package com.cn.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@ServletComponentScan
 @EnableCaching
 @EnableTransactionManagement
 @SpringBootApplication
---
+++ src/main/java/com/cn/school/config/WebAppConfig.java
@@ -0,0 +1,30 @@
+package com.cn.school.config;
+
+import com.cn.school.aop.CommonInterceptorl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author:HuMin Date:2019/3/13
+ * Time:11:19
+ */
+@Configuration
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration addInterceptor = registry.addInterceptor(getSecurityInterceptor());
+        //通過路徑
+//        addInterceptor.excludePathPatterns("/login");
+        //拦截路径
+        addInterceptor.addPathPatterns("/**");
+    }
+
+    @Bean
+    public CommonInterceptorl getSecurityInterceptor() {
+        return new CommonInterceptorl();
+    }
+}
