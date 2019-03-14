0fec72057c99f2b114b95863cec10c5d75fb7825
leiyunlong <1175333149@qq.com>
2019-03-14 10:03:07 +0800

    注释掉过滤器

--- src/main/java/com/cn/school/aop/MyFilter.java
+++ src/main/java/com/cn/school/aop/MyFilter.java
@@ -1,36 +1,36 @@
-package com.cn.school.aop;
-
-import com.cn.school.config.BodyReaderHttpServletRequestWrapper;
-
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-/**
- * Created by yefuliang on 2017/10/25.
- */
-@WebFilter(filterName = "myFilter", urlPatterns = "/*")
-public class MyFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("--------------过滤器初始化------------");
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("--------------执行过滤操作------------");
-
-        // 防止流读取一次后就没有了, 所以需要将流继续写出去
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        ServletRequest requestWrapper = new BodyReaderHttpServletRequestWrapper(httpServletRequest);
-
-        filterChain.doFilter(requestWrapper, servletResponse);
-    }
-
-    @Override
-    public void destroy() {
-        System.out.println("--------------过滤器销毁------------");
-    }
-}
-
+//package com.cn.school.aop;
+//
+//import com.cn.school.config.BodyReaderHttpServletRequestWrapper;
+//
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+///**
+// * Created by yefuliang on 2017/10/25.
+// */
+//@WebFilter(filterName = "myFilter", urlPatterns = "/*")
+//public class MyFilter implements Filter {
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        System.out.println("--------------过滤器初始化------------");
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        System.out.println("--------------执行过滤操作------------");
+//
+//        // 防止流读取一次后就没有了, 所以需要将流继续写出去
+//        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+//        ServletRequest requestWrapper = new BodyReaderHttpServletRequestWrapper(httpServletRequest);
+//
+//        filterChain.doFilter(requestWrapper, servletResponse);
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        System.out.println("--------------过滤器销毁------------");
+//    }
+//}
+//
