e9e7448cd823c90b522aa4f087a28e993ea910ec
mazhijun <1752263458@qq.com>
2019-03-29 15:16:32 +0800

    汉字转首字母大写，随机字符串

--- src/main/java/com/cn/school/utils/RandomCharAndNumUtil.java
+++ src/main/java/com/cn/school/utils/RandomCharAndNumUtil.java
@@ -17,25 +17,20 @@ public class RandomCharAndNumUtil {
             for(int i = 0; i < length; i++) {
                 String num = random.nextInt(2) % 2 == 0 ? "char" : "num";
                 String ch = "char";
-                //输出字母还是数字
+                //输出字母
                 if( "char".equalsIgnoreCase(ch) ) {
                     //输出是大写字母还是小写字母
                     int temp = random.nextInt(2) % 2 == 0 ? 65 : 97;
                     val += (char)(random.nextInt(26) + temp);
                 }
             }
+            //加入随机数字
             for(int i = 0; i < 3; i++) {
                 String num = "num";
                     val += String.valueOf(random.nextInt(10));
                 }
+            //接入时间戳
             val += Calendar.getInstance().getTimeInMillis();
             return val;
         }
-        @Test
-        public  void  test() {
-            RandomCharAndNumUtil test = new RandomCharAndNumUtil();
-         //测试
-            long t4=System.currentTimeMillis();
-            System.out.println(test.getStringRandom());
     }
-}
